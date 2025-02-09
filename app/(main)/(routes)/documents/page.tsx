61ebb64ff3a0687c40ea296c3441a4d5fc68b235
naumaanansari <nomaanansari1115@gmail.com>
2025-02-09 12:24:31 +0530

    Sidebar Completed and User Options implemeted.

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,6 +1,21 @@
+"use client"
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
-    return ( <div className="">
-        This is a protective page.
+    const {user} = useUser();
+    return ( <div className="h-full flex flex-col items-center justify-center space-y-4">
+        <Image src="black-no-answer.svg" height="300" width="300" alt="Empty" className="dark:hidden" />
+        <Image src="white-no-answer.svg" height="300" width="300" alt="Empty" className="hidden dark:block" />
+        <h2 className="text-lg font-medium">
+            Welcome To {user?.firstName}&apos;s Motion
+        </h2>
+        <Button>
+            <PlusCircle className="h-4 w-4 mr-1"/>Create A Note
+        </Button>
     </div> );
 }
  
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,6 +6,7 @@ import {  ChevronsLeft, MenuIcon } from "lucide-react"
 import { usePathname } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import {useMediaQuery} from "usehooks-ts"
+import { UserItem } from "./user-item"
 
 
 export const Navigation = () => {
@@ -112,7 +113,7 @@ export const Navigation = () => {
             <ChevronsLeft className="h-6 w-6"/>
         </div>
         <div className="">
-            <p className="">Action Items</p>
+            <UserItem/>
         </div>
         <div className="mt-4">
             <p className="">Documents</p>
